4dee9c7a6ac9a0fcb51e6422efa2ac55bc5f282a
Dhanasekaran24 <113352772+Dhanasekaran24@users.noreply.github.com>
2022-10-01 21:59:29 +0530

    Add files via upload
    Day1

---
+++ Report.java
@@ -0,0 +1,13 @@
+package week1day1;
+
+public class Report {
+	public static void main(String[] args) {
+		Student s = new Student();
+		System.out.println("Name of the student - "+s.studentName );
+		System.out.println("Roll number - "+ s.rollNo);
+		System.out.println("College name - "+ s.collegeName);
+		System.out.println("Marks scored out of 600 - "+ s.marksScored);
+		System.out.println("cgpa - "+ s.cgpa);
+
+}
+}
---
+++ Student.java
@@ -0,0 +1,10 @@
+package week1day1;
+
+public class Student {
+	String studentName = "dhanasekaran";
+	String rollNo = "19l001";
+	String collegeName = "a.m jain college";
+	int marksScored = 545;
+	float cgpa = 70.45f;
+
+}
