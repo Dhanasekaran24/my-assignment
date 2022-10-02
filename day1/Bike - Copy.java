eb6c9d644abc864e2ce3c86e21c48d9bc34f7dfb
Dhanasekaran24 <113352772+Dhanasekaran24@users.noreply.github.com>
2022-10-02 13:35:38 +0530

    Add files via upload

---
+++ day1/Bike - Copy.java
@@ -0,0 +1,13 @@
+package week1day1;
+
+public class Bike {
+
+		public static void main(String[] args) {
+			Car c = new Car();
+			Bike b = new Bike();
+			c.applyBrake();
+			c.soundHorn();
+
+	}
+
+}
---
+++ day1/Safari.java
@@ -0,0 +1,12 @@
+package week1day1;
+
+public class Safari {
+	public void getName() {
+		System.out.println("This is google chrome");
+	}
+	public void printName() {
+		System.out.println("This is google chrome");
+		
+	}
+
+}
