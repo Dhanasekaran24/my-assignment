db4b3451b9d50c2438316b1f3f520251048b6ce1
Dhanasekaran24 <113352772+Dhanasekaran24@users.noreply.github.com>
2022-10-19 22:06:11 +0530

    Add files via upload

---
+++ week3day2/inter/Execution.java
@@ -0,0 +1,19 @@
+package org.inter;
+
+public class Execution  extends MultiLanguage {
+
+	public static void main(String[] args) {
+		MultiLanguage A = new MultiLanguage();
+		A.Java();
+		A.python();
+		A.javaScript();
+		A.ruby();
+		A.Selenium();
+	
+
+	}
+
+}
+	
+
+
---
+++ week3day2/inter/Language.java
@@ -0,0 +1,12 @@
+package org.inter;
+
+public interface Language {
+	public void Java();
+	
+	
+	public void javaScript();
+
+	public void ruby();
+
+
+}
